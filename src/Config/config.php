f9865f6283986a973f3370f5b9d4274f4976de33
DesHuth <mail@perfecthomecomputers.co.uk>
2025-06-05 14:00:27 +0100

    Update config for tap-3

--- src/Config/config.php
+++ src/Config/config.php
@@ -18,11 +18,22 @@ return [
         'dry_run' => false,
         'projectId' => 'my-project-id',
         'jsonFile' => __DIR__ . '/fcmCredentials/file.json',
-        // Optional: Default Guzzle request options for each FCM request
-        // See https://docs.guzzlephp.org/en/stable/request-options.html
         'guzzle' => [],
     ],
     'apn' => [
+        'certificate' => __DIR__ . '/iosCertificates/apns-dev-cert.pem',
+        'passPhrase' => 'secret', //Optional
+        'passFile' => __DIR__ . '/iosCertificates/yourKey.pem', //Optional
+        'dry_run' => true,
+    ],
+    'fcm-tap-3' => [
+        'priority' => 'normal',
+        'dry_run' => false,
+        'projectId' => 'my-project-id',
+        'jsonFile' => __DIR__ . '/fcmCredentials/file.json',
+        'guzzle' => [],
+    ],
+    'apn-tap-3' => [
         'certificate' => __DIR__ . '/iosCertificates/apns-dev-cert.pem',
         'passPhrase' => 'secret', //Optional
         'passFile' => __DIR__ . '/iosCertificates/yourKey.pem', //Optional
